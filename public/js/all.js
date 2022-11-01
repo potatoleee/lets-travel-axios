3926cc288bb1a2f992e32b2976f639a5142ba65b
potatolee <dean19990226@gmail.com>
2022-11-01 18:14:05 +0800

    數量比對功能完成

--- public/js/all.js
+++ public/js/all.js
@@ -73,7 +73,7 @@ data.forEach(function(item){
     <div class="card h-100 border shadow">
         <div class="position-relative">
             <p class="position-absolute top-m12p start-0 bg-secondary fs-4 text-white py-2 px-4 rounded-end">${item.area}</p>
-            <img src="${item.imgUrl}" alt="綠島自由行套行程">
+            <img class="object-cover w-100 h-45n" src="${item.imgUrl}" alt="綠島自由行套行程">
             <p class="position-absolute bottom-m12p start-0 bg-primary  text-white py-1 px-2 rounded-end">${item.rate}</p>
         </div>
         <div class="bg-white px-5 pt-5 pb-4">
@@ -90,12 +90,19 @@ data.forEach(function(item){
             </div>
         </div>
     </div>
-</li>`
+</li>`;
 });
 ticketResult.innerHTML= str;
+const searchResultNum = ticketResult.getElementsByTagName("li").length;
+console.log(searchResultNum);
+
+const searchResultTotal = document.querySelector(".searchResultTotal");
+searchResultTotal.innerHTML = `<p>本次搜尋共${searchResultNum}筆資料</p>`;
+
+//搜尋總數量 end
 
 }// 初始預設格式 end
-// init();
+init();
 
 //變數都會用到因此拉出函示外面，變成全域變數;
 const locationFilter = document.querySelector(".locationFilter");
@@ -106,12 +113,11 @@ locationFilter.addEventListener("change",function(e){
     let select = e.target.value;
     data.forEach(function(item,index){
         if(select == "全部地區"){
-            let content = `
-            <li class="col-md-6 col-lg-4">
+            let content = `<li class="col-md-6 col-lg-4">
             <div class="card h-100 border shadow">
                 <div class="position-relative">
                     <p class="position-absolute top-m12p start-0 bg-secondary fs-4 text-white py-2 px-4 rounded-end">${item.area}</p>
-                    <img src="${item.imgUrl}" alt="綠島自由行套行程">
+                    <img class="object-cover w-100 h-45n" src="${item.imgUrl}" alt="綠島自由行套行程">
                     <p class="position-absolute bottom-m12p start-0 bg-primary  text-white py-1 px-2 rounded-end">${item.rate}</p>
                 </div>
                 <div class="bg-white px-5 pt-5 pb-4">
@@ -122,7 +128,7 @@ locationFilter.addEventListener("change",function(e){
                             <span class="material-symbols-outlined icon-fill text-primary">
                                 error
                                 </span>
-                                剩下最後 ${item.group} 組
+                             剩下最後 ${item.group} 組
                         </p>
                         <p class="d-flex align-items-center gap-1 text-primary">TWD <span class="font-roboto fs-2">$${item.price}</span></p>
                     </div>
@@ -131,12 +137,11 @@ locationFilter.addEventListener("change",function(e){
         </li>`;
         str += content;
     }else if(select == item.area){
-        let content = `
-        <li class="col-md-6 col-lg-4">
+        let content = `<li class="col-md-6 col-lg-4">
         <div class="card h-100 border shadow">
             <div class="position-relative">
                 <p class="position-absolute top-m12p start-0 bg-secondary fs-4 text-white py-2 px-4 rounded-end">${item.area}</p>
-                <img src="${item.imgUrl}" alt="綠島自由行套行程">
+                <img class="object-cover w-100 h-45n" src="${item.imgUrl}" alt="綠島自由行套行程">
                 <p class="position-absolute bottom-m12p start-0 bg-primary  text-white py-1 px-2 rounded-end">${item.rate}</p>
             </div>
             <div class="bg-white px-5 pt-5 pb-4">
@@ -147,7 +152,7 @@ locationFilter.addEventListener("change",function(e){
                         <span class="material-symbols-outlined icon-fill text-primary">
                             error
                             </span>
-                            剩下最後 ${item.group} 組
+                         剩下最後 ${item.group} 組
                     </p>
                     <p class="d-flex align-items-center gap-1 text-primary">TWD <span class="font-roboto fs-2">$${item.price}</span></p>
                 </div>
@@ -158,6 +163,16 @@ locationFilter.addEventListener("change",function(e){
     }
 })
 ticketResult.innerHTML = str;
+//搜尋總數量 start 
+const searchResultNum = ticketResult.getElementsByTagName("li").length;
+console.log(searchResultNum);
+
+const searchResultTotal = document.querySelector(".searchResultTotal");
+searchResultTotal.innerHTML = `<p>本次搜尋共${searchResultNum}筆資料</p>`;
+
+//搜尋總數量 end
+
+
 });// 地區篩選 end
 
 
@@ -194,19 +209,10 @@ btnAddTicket.addEventListener("click",function(e){
     ticketDescription.value = "";
     ticketNum.value = "";
     ticketPrice.value = "";
-    
-    // {
-    //     "id": 0,
-    //     "name": "綠島自由行套裝行程",
-    //     "imgUrl":"./public/images/travel_1.png",
-    //     "area": "台北",
-    //     "description": "嚴選超高CP值綠島自由行套裝行程，多種綠島套裝組合，提供台東綠島來回船票、綠島環島機車、綠島民宿住宿，行程加贈『綠島浮潛體驗』以及『綠島生態導覽』，讓你用輕鬆的綠島套裝自由行，也能深度認識綠島在地文化。",
-    //     "group": "剩下最後8組",
-    //     "price": 1280,
-    //     "rate": 8.6
-    //   },
+       
+})//新增套票 end
 
 
-   
-})
+
+
 
