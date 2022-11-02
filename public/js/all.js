20877c0c16045e60297b4291fc17c67dcdedfbce
potatolee <dean19990226@gmail.com>
2022-11-02 15:33:35 +0800

    rwd設定

--- public/js/all.js
+++ public/js/all.js
@@ -35,7 +35,7 @@ let data = [
         "imgUrl":"./public/images/travel_3.png",
         "area": "台中",
         "description": "山林悠遊套票，結合南投清境高空步道、雙龍瀑布七彩吊橋、瑞龍瀑布園區之熱門景點，帶您飽覽南投瑰麗的自然環境，體驗變化無窮的地形景觀，喜歡挑戰高空的您一定不可錯過。<br>（含雙龍瀑布入場券 x2）",
-        "group": "限時搶購",
+        "group": "20",
         "price": 880,
         "rate": 9.3
     },
@@ -62,8 +62,11 @@ let data = [
     
   ];
 
-
+// ticketResult 篩選後的全部結果
 const ticketResult = document.querySelector(".ticketResult");
+
+// locationFilter 篩選的按鈕;
+const locationFilter = document.querySelector(".locationFilter");
 
 // 初始預設格式 start
 function init(){
@@ -104,16 +107,14 @@ searchResultTotal.innerHTML = `<p>本次搜尋共${searchResultNum}筆資料</p>
 }// 初始預設格式 end
 init();
 
-//變數都會用到因此拉出函示外面，變成全域變數;
-const locationFilter = document.querySelector(".locationFilter");
+
 
 // 地區篩選 start
 locationFilter.addEventListener("change",function(e){
     let str = "";
     let select = e.target.value;
     data.forEach(function(item,index){
-        if(select == "全部地區"){
-            let content = `<li class="col-md-6 col-lg-4">
+        let content = `<li class="col-md-6 col-lg-4">
             <div class="card h-100 border shadow">
                 <div class="position-relative">
                     <p class="position-absolute top-m12p start-0 bg-secondary fs-4 text-white py-2 px-4 rounded-end">${item.area}</p>
@@ -135,38 +136,19 @@ locationFilter.addEventListener("change",function(e){
                 </div>
             </div>
         </li>`;
+        if(select == "全部地區"){
         str += content;
-    }else if(select == item.area){
-        let content = `<li class="col-md-6 col-lg-4">
-        <div class="card h-100 border shadow">
-            <div class="position-relative">
-                <p class="position-absolute top-m12p start-0 bg-secondary fs-4 text-white py-2 px-4 rounded-end">${item.area}</p>
-                <img class="object-cover w-100 h-45n" src="${item.imgUrl}" alt="綠島自由行套行程">
-                <p class="position-absolute bottom-m12p start-0 bg-primary  text-white py-1 px-2 rounded-end">${item.rate}</p>
-            </div>
-            <div class="bg-white px-5 pt-5 pb-4">
-                <h3 class="fw-medium fs-3 mb-4 pb-1 border-bottom border-2 border-primary text-primary ">${item.name}</h3>
-                <p class="mb-7 pb-7">${item.description}</p>
-                <div class="d-flex align-items-center justify-content-between position-absolute bottom-16p start-0 w-100 px-5">
-                    <p class="d-flex align-items-center gap-1 text-primary">
-                        <span class="material-symbols-outlined icon-fill text-primary">
-                            error
-                            </span>
-                         剩下最後 ${item.group} 組
-                    </p>
-                    <p class="d-flex align-items-center gap-1 text-primary">TWD <span class="font-roboto fs-2">$${item.price}</span></p>
-                </div>
-            </div>
-        </div>
-    </li>`;
-    str += content;
-    }
+        }else if(select == item.area){
+         str += content;
+        }
+        console.log(index);
 })
 ticketResult.innerHTML = str;
-//搜尋總數量 start 
-const searchResultNum = ticketResult.getElementsByTagName("li").length;
-console.log(searchResultNum);
+//搜尋總數量 start
 
+
+const searchResultNum = ticketResult.getElementsByTagName("li").length;
+// 計算有幾個li
 const searchResultTotal = document.querySelector(".searchResultTotal");
 searchResultTotal.innerHTML = `<p>本次搜尋共${searchResultNum}筆資料</p>`;
 
